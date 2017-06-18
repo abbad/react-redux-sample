2da58a78ae5546a6350864d9c38af200fdc58537
abbad <ahm.abbad@gmail.com>
2017-06-18 15:50:04 -0400

    WIP: Added new dependencies, created app, server.js, and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+var BUILD_DIR = path.join(__dirname, '/dist/');
+
+var config = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'react-hot-loader/patch',
+    // TODO: Error: [HMR] Hot Module Replacement is disabled. (investigate)
+    // 'webpack/hot/only-dev-server',
+    path.resolve(__dirname, 'app/index.jsx')
+  ],
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+          template: 'app/index.html',
+          inject: 'body',
+          filename: 'index.html'
+        })]
+};
+
+module.exports = config;
