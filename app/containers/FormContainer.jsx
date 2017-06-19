f85546380f7e11d5a07c380bd47167b10f5881ce
abbad <ahm.abbad@gmail.com>
2017-06-19 19:02:08 -0400

    WIP: React router is in place, needs to be taken out from index, created containers folder that has inital jsx files that later will be altered to do something usefull

---
+++ app/containers/FormContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+export default class FormContainer extends React.Component {
+  render () {
+    return (<div>Where are you located?</div>);
+  }
+}
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,32 @@
+// insert into index.js
+import About from './containers/AboutContainer.jsx';
+import FormContainer from './containers/FormContainer.jsx';
+
 import React from 'react';
 import {render} from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/get-started">Get Started</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route path="/get-started" component={FormContainer}/>
+          <Route path="/about" component={About}/>
+        </div>
+      </Router>
+    );
   }
 }
 
