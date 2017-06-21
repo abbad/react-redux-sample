e3dc27cf07f6d548e76baac553344cbcb97fb064
abbad <ahm.abbad@gmail.com>
2017-06-21 17:50:09 -0400

    A simple form has been added, sass is not working, I think its the loads.

--- app/containers/FormContainer.jsx
+++ app/containers/FormContainer.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 import {render} from 'react-dom';
 
 export default class FormContainer extends React.Component {
+
+
+  handleChange(event) {
+    console.log(event);
+  }
+
   render () {
-    return (<div>Where are you located?</div>);
+    return (<div className='form_container'>
+      Where are you located?
+      <form>
+        <label>
+          Address Line One:
+            <input type="text" name="addressLineOne" />
+          </label>
+          <label>
+            Address Line Two:
+              <input type="text" name="addressLineTwo" />
+            </label>
+            <label>
+              City
+              <input type="text" name="city" />
+            </label>
+            <label>
+              State
+                <input type="text" name="state" />
+              </label>
+              <label>
+                ZIP|Postal Code:
+                  <input type="text" name="zipcode" />
+              </label>
+              <select value="USA" onChange={this.handleChange}>
+                  <option value="USA">USA</option>
+              </select>
+          <input type="button" value="Next" />
+        </form>
+    </div>);
   }
 }
